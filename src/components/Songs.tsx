67c0a5bd790db79ddbf6ebed350ea9a504dea23c
samfreeman <sam.freeman.55@gmail.com>
2023-01-03 01:10:37 -0500

    Removed dbg border from Layout and Songs

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -5,7 +5,7 @@ import Song from './Song'
 
 export const Songs = () => {
 	return (
-		<div className='flex flex-col flex-wrap content-start max-h-full gap-2 overflow-auto border-2 border-red-400 border-solid'>
+		<div className='flex flex-col flex-wrap content-start max-h-full gap-2 overflow-auto'>
 			{data.map(song => <Song key={song.id} song={song} />)}
 		</div>
 	)
