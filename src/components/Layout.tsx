eb1972fe14c71a13589e73d945744293e99a321c
samfreeman <sam.freeman.55@gmail.com>
2023-01-03 02:08:29 -0500

    Started to add bg image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,25 @@
 
+import Image from 'next/image'
+
 import { DivProps } from '@client/DomProps'
 
 
-export const Layout = ({ children }: DivProps) => (
-	<div className='flex flex-col max-h-screen min-h-screen px-8 py-4 overflow-hidden font-body bg-zinc-800 text-slate-300'>
-		{children}
-	</div>
-)
+export const Layout = ({ children }: DivProps) => {
+	return (
+		<div
+			className={
+				'flex flex-col '
+				+ 'max-h-screen min-h-screen '
+				+ 'px-8 py-4 '
+				+ 'overflow-hidden '
+				+ 'font-body bg-zinc-800 text-slate-300 '
+				//+ 'bg-ut-logo'
+			}
+		>
+			{children}
+		</div>
+	)
+}
 
 
 export default Layout
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,17 @@ module.exports = {
 	content: [
 		"./src/components/**/*.{js,ts,jsx,tsx}",
 		"./src/controls/**/*.{js,ts,jsx,tsx}",
-		"./src/pages/**/*.{js,ts,jsx,tsx}"
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./public/**/*.svg"
 	],
 	theme: {
 		extend: {
 			fontFamily: {
 				"body": ["Rubik", "sans-serif"]
-			}
+			},
+			// backgroundImage: {
+			// 	"ut-logo": "url('ut-logo.svg')"
+			// }
 		}
 	},
 	plugins: []
