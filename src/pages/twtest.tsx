0d774ded906db49e6c3dfd769b368de410c1f0d2
samfreeman <sam.freeman.55@gmail.com>
2023-01-02 20:05:16 -0500

    Added tailwindcss

---
+++ src/pages/twtest.tsx
@@ -0,0 +1,17 @@
+
+const Test = () => {
+	const message = new class { x }().hasOwnProperty('x') ?
+		'Transpiler is configured correctly'
+		: 'Transpiler is not configured correctly'
+
+	const color = message.includes('not')
+		? 'text-red-400'
+		: 'text-green-400'
+	return (
+		<div className={`m-4 text-lg ${color}`}>
+			{message}
+		</div>
+	)
+}
+
+export default Test
