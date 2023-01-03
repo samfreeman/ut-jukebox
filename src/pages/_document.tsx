2d0554ad5e07143939be7e12ebc00561f809bbf2
samfreeman <sam.freeman.55@gmail.com>
2023-01-02 23:56:59 -0500

    Added font link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
