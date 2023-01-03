d53c77cdff66386ce524d8c0d88dd56f07266c3e
samfreeman <sam.freeman.55@gmail.com>
2023-01-02 23:55:53 -0500

    Added Layout page and font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { DivProps } from '@client/DomProps'
 
 
 export const Layout = ({ children }: DivProps) => (
-	<div className='border-spacing-4'>
+	<div className='flex flex-col max-h-screen min-h-screen px-8 py-4 overflow-hidden border-2 border-green-400 border-solid font-body bg-zinc-800 text-slate-300'>
 		{children}
 	</div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
 		"./src/pages/**/*.{js,ts,jsx,tsx}"
 	],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				"body": ["Rubik", "sans-serif"]
+			}
+		}
 	},
 	plugins: []
 }
